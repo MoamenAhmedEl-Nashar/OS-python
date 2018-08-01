4d6c3ad7a7c581f28a1896076bfde51764dfcedd
Moamen Ahmed <moamen.ahmed1@hotmail.com>
2018-08-01 23:44:40 +0200

    completing the clean up desktop app

--- OS script/OS_script.py
+++ OS script/OS_script.py
@@ -1,4 +1,9 @@
 import os
-dir='C:\\Users\\merav\\Desktop'
-
+import shutil
+dir='C:\\Users\\merav\\Desktop\\newFolderCreatedByPython'
+os.makedirs(dir,511,True)
+lis=os.listdir('C:\\Users\\merav\\Desktop')
+for i in lis:
+    file='C:\\Users\\merav\\Desktop\\'+i
+    shutil.move(file,dir)
 
